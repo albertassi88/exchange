b472574538bb72e3c57e386108d6b431aa9b2802
albertassi88 <albertassi88@gmail.com>
2025-10-28 20:55:55 -0300

    feat(exchange): add currency-search component

--- src/app/features/exchange/components/currency-search/currency-search.component.ts
+++ src/app/features/exchange/components/currency-search/currency-search.component.ts
@@ -1,8 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-currency-search',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule
+    ],
   templateUrl: './currency-search.component.html',
   styleUrl: './currency-search.component.scss'
 })
--- src/app/features/exchange/pages/exchange-page/exchange-page.component.ts
+++ src/app/features/exchange/pages/exchange-page/exchange-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CurrencySearchComponent } from '../../components/currency-search/currency-search.component';
+import { CurrencyCardComponent } from '../../components/currency-card/currency-card.component';
 
 @Component({
   selector: 'app-exchange-page',
-  imports: [],
+  imports: [CurrencySearchComponent,CurrencyCardComponent],
   templateUrl: './exchange-page.component.html',
   styleUrl: './exchange-page.component.scss'
 })
