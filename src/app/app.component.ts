848e4f62e7e9f066fa013095a22899e2f39d7231
albertassi88 <albertassi88@gmail.com>
2025-10-28 20:10:42 -0300

    feat: create Exchange page with layout and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ExchangePageComponent } from './features/exchange/pages/exchange-page/exchange-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ExchangePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiKey: 'RVZG0GHEV2KORLNA',
+  apiUrl: 'https://api-brl-exchange.actionlabs.com.br/api/1.0/open'
+};
