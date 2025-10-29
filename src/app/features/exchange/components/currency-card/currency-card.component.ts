f0a9b0fc9926fb54205519697f246d79e344d7e1
albertassi88 <albertassi88@gmail.com>
2025-10-28 22:37:38 -0300

    feat(exchange): add currency-card component

--- src/app/features/exchange/components/currency-card/currency-card.component.ts
+++ src/app/features/exchange/components/currency-card/currency-card.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-currency-card',
-  imports: [],
+  imports: [MatCardModule ],
   templateUrl: './currency-card.component.html',
   styleUrl: './currency-card.component.scss'
 })
